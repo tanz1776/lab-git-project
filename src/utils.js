27090959fdf0ca43b6e81b0a348a74c9bb970765
tanz1776 <ew.petrenko09@gmail.com>
2025-01-28 23:28:20 +0300

    Added addNumbers function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
-function placeholder() {
-    return "This is a placeholder";
+function addNumbers(a, b) {
+    return a + b;
 }
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+module.exports = { addNumbers, multiplyNumbers };
