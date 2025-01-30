2dc94637c4a3b5ce31e4f479cf216563884dcef8
tanz1776 <ew.petrenko09@gmail.com>
2025-01-30 04:50:24 +0300

    Merged stashed changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 const { addNumbers, multiplyNumbers } = require("./utils");
 
-console.log(addNumbers(2, 3));
-console.log(multiplyNumbers(2, 3));
+console.log(addNumbers(25, 3));
+console.log(multiplyNumbers(25, 3));
