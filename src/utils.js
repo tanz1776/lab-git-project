56a28d3284e5ac4e5ad3d0992367cb497e4e3833
tanz1776 <ew.petrenko09@gmail.com>
2025-01-30 05:10:52 +0300

    Added divideNumbers function

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,10 @@ function multiplyNumbers(a, b) {
     return a * b;
 }
 module.exports = { addNumbers, multiplyNumbers };
+
+function divideNumbers(a, b) {
+    if (b === 0) {
+        return "Cannot divide by zero";
+    }
+    return a / b;
+}
